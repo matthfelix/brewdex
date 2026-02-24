80b925bcba38f481b660b0edfd3dc918baa705f9
Matheus Felix <matheusfelix0996@gmail.com>
2026-02-24 18:19:04 -0300

    fix: remove h-screen mobile issue

--- src/ui/renderStyleFilters.js
+++ src/ui/renderStyleFilters.js
@@ -13,13 +13,13 @@ export function renderStyleFilters(container, styles, selectedStyle) {
     const isActive = style === selectedStyle
 
     btn.className = `
-      px-3 py-2 rounded-lg border text-base font-semibold transition
-      ${
-        isActive
-          ? "bg-amber-500 text-white border-amber-600"
-          : "bg-white text-black border-black hover:bg-amber-100"
-      }
-    `
+    px-3 py-1 rounded-full border text-sm font-medium transition
+    ${
+      isActive
+        ? "bg-amber-500 text-white border-amber-500"
+        : "bg-white text-slate-700 border-slate-300 hover:bg-amber-100"
+    }
+`
 
     container.appendChild(btn)
   })
